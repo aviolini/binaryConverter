3530d7adef512c5cc62d6798583e72fa46d172f2
aviolini <arrigo.violini@gmail.com>
2021-10-04 07:57:25 +0200

    o

--- dec.c
+++ dec.c
@@ -10,50 +10,38 @@ int check(char *s)
 	int c = -1;
 	while (s[++c])
 		if (s[c] != '1' && s[c] != '0')
-		{
-			printf("Bad argument\n");
 			return 1;
-		}
 	return 0;
-}
-
-long int convert (int n, int space)
-{
-	long int ret = 0;
-	if (n != 0)
-		ret = convert(n / 10, ++space);
-	if (n)
-		ret += (n % 2) << (space--);
-	return ret;
 }
 
 int main(int ac, char **av)
 {
-	static int space = -1;
+	int space = 0;
 	
 	if (ac < 2)
 	{
 		printf("Error arguments, enter number/s to converter\n");
 		return 1;
 	}
+	int maxLen = 0;
+	int len;
+	for (int i = 0; i < ac; i++)
+		if ((len = (int)strlen(av[i])) > maxLen)
+			maxLen = len;
+	printf("MAXLEN:%d\n", maxLen);
 	for (int i = 1; i < ac ; i++)
 	{
 		long int ret = 0;
-		size_t size = strlen(av[i]);
 		if (check(av[i]))
+		{
+			printf("[%s]: Bad argument\n",av[i]);
 			continue;
-		printf("ok\n");
-
-		// printf("[%ld]:\t", n);
-		// if (n < 0)// || n > INT_MAX) // || n < INT_MIN TODO
-		// {
-		// 	printf("Bad argument\n");
-		// 	return 1;
-		// }
-		// ret = convert((int)n, space);
-		// // if (!n)
-		// 	printf("%d\n", ret);
-		// // printf("\n");
+		}
+		len = (int)strlen(av[i]);
+		int c = -1;
+		while (av[i][++c])
+		ret += ((av[i][c] + 48 ) % 2) << (--len);
+		printf("[%s]:\t%ld\n", av[i], ret);
 	}
 	return 0;
 }
