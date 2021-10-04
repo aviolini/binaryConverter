ec664cf1179df614fffce36558a1ff7021b658bc
aviolini <arrigo.violini@gmail.com>
2021-10-04 23:09:56 +0200

    ok

--- bits.c
+++ bits.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <string.h>
 #define BYTE 8
 #define INT ((sizeof(char)*(BYTE)) * sizeof(int))
+#define TAB_SIZE 8
+#define EXTRA_CHARS 3
+
+int numlen(int n)
+{
+	int len = 0;
+	for (; n > 0; n /= 10, len++){}
+	return len;
+}
+
+void tab(int n, int maxLen)
+{
+	int len = numlen(n) + EXTRA_CHARS;
+	while ((len>>3) <= (maxLen>>3))
+	{
+		len = len + TAB_SIZE;
+		printf("\t");
+	}
+}
 
 long int convert (int n, int space)
 {
@@ -38,17 +58,32 @@ int main(int ac, char **av)
 		printf("Error arguments, enter number/s to converter\n");
 		return 1;
 	}
+	int maxLen = 0;
+	for (int i = 1; i < ac ; i++)
+	{
+		int c = 0;
+		long int n = atol(av[i]);
+		for (; n > 0; n /= 10, c++){}
+		if (c > maxLen)
+			maxLen = c;
+	}
+	maxLen += EXTRA_CHARS;
 	for (int i = 1; i < ac ; i++)
 	{
 		long int n = atol(av[i]);
-		printf("[%ld]:\t", n);
-		if (n < 0 || n > INT_MAX) // || n < INT_MIN TODO
+		if (n < 0 || n > INT_MAX || (n == 0 && strcmp(av[i], "0"))) // n < 0 TODO
 		{
+			printf("[%s]:", av[i]);
+			tab(n, maxLen);
 			printf("Bad argument\n");
-			return 1;
 		}
-		convert((int)n, space);
-		printf("\n");
+		else 
+		{
+			printf("[%ld]:", n);
+			tab(n, maxLen);
+			convert((int)n, space);
+			printf("\n");
+		}
 	}
 	return 0;
 }
